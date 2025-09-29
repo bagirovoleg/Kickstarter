65d09438eca170c4cbb11ece7c969cab910364b8
bagirovoleg <o.bagirov.pl@gmail.com>
2025-09-29 17:42:59 +0200

    feat: Implement working version of header

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
-  extends: "@mate-academy/stylelint-config",
-  plugins: [
-    "stylelint-scss"
-  ],
-  rules: {}
+  extends: '@mate-academy/stylelint-config',
+  plugins: ['stylelint-scss'],
+  // Ignore shared utils from other projects to avoid property-order conflicts
+  ignoreFiles: ['src/styles/utils/**'],
+  rules: {},
 };
